76b25ab32be1b298d69da13d6ac4f0f003797c8f
qmetivier <quentin.metivier@epsi.fr>
2020-01-28 14:02:00 +0100

    add CodePromo Constructor and request to get this from API

--- lib/main.dart
+++ lib/main.dart
@@ -1,7 +1,10 @@
+import 'dart:convert';
+
+import 'package:flutter/foundation.dart';
 import 'package:flutter/material.dart';
 import 'package:barcode_scan/barcode_scan.dart';
 import 'package:flutter/services.dart';
-import 'package:flutter/scheduler.dart';
+import 'package:http/http.dart' as http;
 
 void main() => runApp(MyApp());
 
@@ -9,11 +12,11 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Wesh',
       theme: ThemeData(
         primarySwatch: Colors.green,
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      home: MyHomePage(title: 'The Wesh Scanning Application'),
     );
   }
 }
@@ -27,12 +30,48 @@ class MyHomePage extends StatefulWidget {
   _MyHomePageState createState() => _MyHomePageState();
 }
 
+class CodePromo {
+
+  String name = '';
+  String code = '';
+  DateTime startDate = DateTime.now();
+  DateTime endDate = DateTime.now();
+
+  CodePromo({this.name, this.code, this.startDate, this.endDate});
+
+  factory CodePromo.fromJson(Map<String, dynamic> json) {
+    return CodePromo(
+      name: json["name"],
+      code: json["code"],
+      startDate: json["create_time"],
+      endDate: json["end_time"],
+    );
+  }
+
+}
+
 class _MyHomePageState extends State<MyHomePage> {
 
   String resultQR = '';
+  List<CodePromo> codePromos = [];
+
+  Future<List<CodePromo>> getCodePromosAPI() async
+  {
+    final response = await http.get("http://192.168.1.16:8008/api/codepromo/");
+
+    if(response.statusCode == 200){
+      json.decode(response.body).forEach((c) {
+       codePromos.add(CodePromo.fromJson(c));
+     });
+    }else{
+      throw Exception('failed to load post');
+    }
+
+  }
 
   Future _scanQR() async
   {
+
     try{
       String qrResult = await BarcodeScanner.scan();
       setState(() {
@@ -63,6 +102,7 @@ class _MyHomePageState extends State<MyHomePage> {
 
   @override
   Widget build(BuildContext context) {
+
     return Scaffold(
       appBar: AppBar(
         title: Text(widget.title),
@@ -71,8 +111,13 @@ class _MyHomePageState extends State<MyHomePage> {
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           children: <Widget>[
-            Text(
-              resultQR
+            ListView.builder(
+              itemCount: codePromos.length,
+              itemBuilder: (context, index){
+                return Card(
+                  child: Text(codePromos[index].code),
+                );
+              }
             )
           ],
         ),
