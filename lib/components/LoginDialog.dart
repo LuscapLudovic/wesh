520a838894d3e86d1b4cdf07a3f1c5b687043b09
qmetivier <quentin.metivier@epsi.fr>
2020-02-04 11:05:35 +0100

    rename error message and reorganize the code

--- lib/components/LoginDialog.dart
+++ lib/components/LoginDialog.dart
@@ -1,72 +1,91 @@
 import 'dart:convert';
-
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
-
 import 'package:http/http.dart' as http;
-import 'package:wesh/components/ErrorDialog.dart';
 
-class LoginDialog{
-  static TextEditingController _username = TextEditingController();
-  static TextEditingController _password = TextEditingController();
+import 'package:wesh/components/ErrorDialog.dart';
+import 'package:wesh/main.dart';
+
+class LoginDialog extends State<MyHomePage>{
+
+  TextEditingController _username = TextEditingController();
+  TextEditingController _password = TextEditingController();
   static String token = '';
 
-  static Future<String> loginDialogShow(BuildContext context) async {
+  @override
+  Widget build(BuildContext context) {
+    return AlertDialog(
+      title: new Text('Authentification'),
+      content: ListView(
+        children: <Widget>[
+          TextField(
+            controller: _username,
+            decoration: InputDecoration(hintText: 'Username'),
+          ),
+          TextField(
+            controller: _password,
+            decoration: InputDecoration(hintText: 'Password'),
+          ),
+        ],
+      ),
+      actions: <Widget>[
+        // usually buttons at the bottom of the dialog
+        new FlatButton(
+          child: new Text("Login"),
+          onPressed: () {
+            _loginApi(context).then((isConnected) => {
+              if(isConnected) Navigator.pop(context, 'success')
+            });
+          },
+        ),
+      ],
+    );
+  }
+
+  Future<String> loginDialogShow(BuildContext context) async {
 
     String value = await showDialog(
         context: context,
         barrierDismissible: false,
-        builder: (BuildContext context) {
-          // return object of type Dialog
-          return AlertDialog(
-            title: new Text('Authentification'),
-            content: ListView(
-              children: <Widget>[
-                TextField(
-                  controller: _username,
-                  decoration: InputDecoration(hintText: 'Username'),
-                ),
-                TextField(
-                  controller: _password,
-                  decoration: InputDecoration(hintText: 'Password'),
-                ),
-              ],
-            ),
-            actions: <Widget>[
-              // usually buttons at the bottom of the dialog
-              new FlatButton(
-                child: new Text("Login"),
-                onPressed: () {
-                  _loginApi(context).then((isConnected) => {
-                    Navigator.pop(context, 'success')
-                  });
-                },
-              ),
-            ],
-          );
-        },
+        builder: build,
       );
 
     return value;
 
     }
 
-    static Future<bool> _loginApi(BuildContext context) async {
-      http.Response response = await http.post(
+    Future<bool> _loginApi(BuildContext context) async {
+      try {
+        http.Response response = await http.post(
             'http://192.168.43.2:8008/api/token-auth/',
             headers: {"Content-Type": "application/json"},
-            body: json.encode({'username': _username.text, 'password': _password.text})
-        ).timeout(Duration(seconds: 5));
+            body: json.encode(
+                {'username': _username.text, 'password': _password.text})
+        ).timeout(Duration(seconds: 5)).catchError((onError) => {
+          debugPrint(onError.toString())
+        });
 
-      if(response.statusCode != 200){
-        ErrorDialog('Error Auth', 'Username or Password is wrong', context);
-      }else{
-        token = "Token " + json.decode(response.body)['token'];
+        switch (response.statusCode) {
+          case 200:
+            token = "Token " + json.decode(response.body)['token'];
+            break;
+          case 400:
+            ErrorDialog('Erreur Authentification',
+                'Identifiant et/ou mot de passe erroné(s)', context);
+            break;
+          case 404:
+            ErrorDialog('Erreur API', "Impossible d'acceder à l'API", context);
+            break;
+          default:
+            ErrorDialog('Erreur Inconnue', "Une erreur inconnue c'est produite", context);
+        }
+
+        return (response.statusCode == 200);
       }
-
-      debugPrint(token);
-
-      return (response.statusCode == 200);
+      catch(exception){
+        ErrorDialog('Erreur Réseau', "Impossible d'acceder au reseau", context);
+        return false;
+      }
     }
+
 
 }
